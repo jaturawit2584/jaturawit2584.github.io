c9033ed82642717a7e133214e63ee228dd413d7e
jaturawit2584 <jaturawit@kkumail.com>
2021-08-25 16:41:30 +0700

    Prime

---
+++ p1.js
@@ -0,0 +1,28 @@
+function showPrimes(n) {
+
+    let listNumber = []
+    for (let i = 2; i < n; i++) {
+        if (!isPrimes(i)) continue;
+        listNumber.push(i)
+    }
+    alert(`For n = ${number} prime numbers are ${listNumber}`)
+}
+
+function isPrimes(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) return false;
+    }
+    return true;
+
+}
+
+let number = Number(prompt("Enter a positive integer "))
+while (true) {
+    if (Number.isInteger(number) && number > 2) {
+        showPrimes(number)
+        break
+    } else {
+        number = Number(prompt("Enter a positive integer"))
+    }
+
+}
